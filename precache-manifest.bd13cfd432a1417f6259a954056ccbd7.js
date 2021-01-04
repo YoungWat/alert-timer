9cd799173fc4d574ec4e2f425d58f628bef4c349
松鼠 <liuyang05@innotechx.com>
2021-01-04 16:02:43 +0800

    feat: react html替换主页

---
+++ precache-manifest.bd13cfd432a1417f6259a954056ccbd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145bafa4c35f04922ef513c8449763c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c6ef163e76a4a6c38e2",
+    "url": "/static/css/main.13014c98.chunk.css"
+  },
+  {
+    "revision": "d03a61034571713be6b5",
+    "url": "/static/js/2.bbc191fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bbc191fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6ef163e76a4a6c38e2",
+    "url": "/static/js/main.04f897bd.chunk.js"
+  },
+  {
+    "revision": "e2677a27ff29970632d9",
+    "url": "/static/js/runtime-main.08af0fed.js"
+  }
+]);
